a57a4a7012874b9c1e9e8e9d4fa2d415d67b9c8d
Boxfriend <cawills394@gmail.com>
2021-08-28 06:07:12 -0400

    Removed unnecessary scriptable object
    i had originally used a scriptable object to hold the sprites for the house prefab but now that the animator is being used it is no longer necessary

--- Assets/BrackeysGameJam/Scripts/ObstacleSprite.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName ="ObstacleSprite.asset",menuName ="ScriptableObject/ObstacleSprite")]
-public class ObstacleSprite : ScriptableObject
-{
-    public Sprite[] sprites;
-}
